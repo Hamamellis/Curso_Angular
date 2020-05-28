3da39bec55c86de3368101ca9418b2762ecda209
Hamamellis <you@example.com>
2020-05-28 20:55:48 -0300

    Aprendemos a diferenciar o Typescript Angular pelo Transpiler do Código Javascript Vanilla (javascript puro)

---
+++ hello-typescript/main.ts
@@ -0,0 +1,21 @@
+var minhaVar = 'minha variavel';
+
+    function minhaFunc (x, y) {
+        return x + y;
+    }
+
+    // ES6 ou ES 2015...
+    //let num = 2;
+
+    //const PI = 3.14;
+
+    const PI = 3.141593;
+    PI >  3.00;
+
+var numeros = [1, 2, 3, 4, 5, 6];
+
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2);
